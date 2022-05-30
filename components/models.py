b4d140441c1509cb18fc7362e475327bbeb6e845
log-box <a.y.logvin@gmail.com>
2022-05-30 21:22:35 +0300

    lesson4 mvp v0.1

--- components/models.py
+++ components/models.py
@@ -1,3 +1,6 @@
+import quopri
+
+
 class User:
     pass
 
@@ -26,3 +29,76 @@ class UserFactory:
         return cls.types[type_]
 
 
+class Board:
+    def __init__(self, name, category):
+        self.name = name
+        self.category = category
+        self.category.boards.append(self)
+
+
+class Category:
+    category_id = 0
+
+    def __init__(self, name, category):
+        self.name = name
+        self.id = Category.category_id
+        Category.category_id += 1
+        self.category = category
+        self.boards = []
+
+
+class PublicBoard(Board):
+    pass
+
+
+class PrivateBoard(Board):
+    pass
+
+
+class BoardFactory:
+    types = {
+        'public': PublicBoard,
+        'private': PrivateBoard,
+    }
+
+    @classmethod
+    def create(cls, type_):
+        return cls.types[type_]
+
+
+class CoreEngine:
+    def __init__(self):
+        self.guest = []
+        self.register = []
+        self.admin = []
+        self.boards = []
+        self.categories = []
+
+    @staticmethod
+    def create_user(type_):
+        return UserFactory.create(type_)
+
+    @staticmethod
+    def create_category(name, category=None):
+        return Category(name, category)
+
+    def find_category(self, id):
+        for item in self.categories:
+            if item.id == id:
+                return item
+
+    @staticmethod
+    def create_board(type_):
+        return BoardFactory.create(type_)
+
+    def get_board(self, name):
+        for item in self.boards:
+            if item.name == name:
+                return item
+        return None
+
+    @staticmethod
+    def decode_value(value):
+        value_b = bytes(value.replace('%', '=').replace('+', ''), 'UTF-8')
+        value_decode_str = quopri.decodestring(value_b)
+        return value_decode_str.decode('UTF-8')
--- views.py
+++ views.py
@@ -1,11 +1,32 @@
+from components.models import CoreEngine
 from logbox_framework.templator import render
+
+core = CoreEngine()
 
 
 class Index:
     def __call__(self):
-        return '200 OK', render('index.html')
+        return '200 OK', render('index.html', objects_list=core.categories)
 
 
 class About:
     def __call__(self):
         return '200 OK', render('about.html')
+
+
+class CreateCategory:
+    def __call__(self, request):
+        if request['method'] == 'POST':
+            name = core.decode_value(request['data']['name'])
+            category_id = request['data'].get('id')
+            category = None
+
+            if category_id:
+                category = core.find_category(int(category_id))
+            new_category = core.create_category(name, category)
+            core.categories.append(new_category)
+
+            return '200 OK', render('index.html', object_list=core.categories)
+        else:
+            categories = core.categories
+            return '200 OK', render('index.html', categories=categories)
